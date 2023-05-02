7b745f85ceece165b1028029044c018334ed412c
d3vjh <jdmorenob@udistrital.edu.co>
2023-05-02 01:35:07 -0500

    Menu implemented + Query database read only

--- HabitabilityBuilding.py
+++ HabitabilityBuilding.py
@@ -39,20 +39,32 @@ class Database:
         """Method to close the database connection."""
         self.connection.close()
 
+"""A global instance is created to access the database"""
+db = Database.getInstance()
+
+
+"""Function to manage the output flow when Ctrl+C is pressed"""
+def sig_handler(sig, frame):
+        print("\n\n[!] Exiting ...")
+        print("[!] Closing Instance with the database")
+        print(" Bye! ")
+        db.closeConnection()
+        sys.exit(1)
+
+signal.signal(signal.SIGINT, sig_handler)
 
 
 class Apartment:
     def __init__(self, numberApartment, airHumidity, ambientAirTemperature, apartmentMaterial, roomQuantity,
-                 personQuantity, personClothing):
+                 personQuantity, isHabitability):
         self.numberApartment = numberApartment
         self.airHumidity = airHumidity
         self.ambientAirTemperature = ambientAirTemperature
         self.apartmentMaterial = apartmentMaterial
         self.roomQuantity = roomQuantity
         self.personQuantity = personQuantity
-        self.personClothing = personClothing
         self.neighbors = []
-        self.isHabitability = True
+        self.isHabitability = isHabitability
 
     def addNeighbor(self, neighbor):
         if neighbor not in self.neighbors:
@@ -62,13 +74,12 @@ class Apartment:
         print(f'[+]==== Info del apartamento: {self.numberApartment}====[+]\n\n'
               f''
               f'[+] Es Habitable: {self.isHabitability} \n'
-              f'[+] Vecinos: {self.neighbors} \n'
+             #f'[+] Vecinos: {self.neighbors} \n'
               f'[+] Humedad del aire:  {self.airHumidity} \n'
               f'[+] Temperatura ambiental: {self.ambientAirTemperature} \n'
               f'[+] Material del apartamento: {self.apartmentMaterial} \n'
               f'[+] Tamaño del apartamento: {self.roomQuantity} \n'
-              f'[+] Cantidad de personas: {self.personQuantity} \n'
-              f'[+] Material de la ropa: {self.personClothing}\n')
+              f'[+] Cantidad de personas: {self.personQuantity} \n')
 
 
 class Building:
@@ -82,13 +93,18 @@ class Building:
                                                          apartmentMaterial, roomQuantity, personQuantity,
                                                          personClothing)
         else:
-            print("El apartamento ya existe")
+            print("The apartment already exists")
             self.Apartments[numberApartment].getInfo()
 
     def addNeighbor(self, numberApartment1, numberApartment2):
         if numberApartment1 in self.Apartments and numberApartment2 in self.Apartments:
             self.Apartments[numberApartment1].addNeighbor(numberApartment2)
             self.Apartments[numberApartment2].addNeighbor(numberApartment1)
+
+
+
+"""A global instance is created to access the Building"""
+building = Building()
 
 
 class Person:
@@ -102,27 +118,28 @@ class Person:
     def setWork(self, work):
         self.work = work
 
+
 class BDD:
 
+
+#Read
     @staticmethod
     def loadApartments(build):
-        file = open("Apartments.txt", "r")
-        content = file.readlines()
-        for line in content:
-            apartment = line.split('\t')
-            numberApartment = apartment[0]
-            airHumidity = apartment[1]
-            ambientAirTemperature = apartment[2]
-            apartmentMaterial = apartment[3]
-            roomQuantity = apartment[4]
-            personQuantity = apartment[5]
-            personClothing = apartment[6]
+        db = Database.getInstance()
+        result = db.executeQuery("SELECT k_idapartment, q_airhumidity, q_ambientairtemperature, n_aparmentmaterial, q_quiantityroom, q_quantityperson, b_ishabitability FROM apartments;")
 
+        for i in range(len(result)):
+            numberApartment = result[i][0]
+            airHumidity = result[i][1]
+            ambientAirTemperature = result[i][2]
+            apartmentMaterial = result[i][3]
+            roomQuantity = result[i][4]
+            personQuantity = result[i][5]
+            isHabitability = result[i][6]
             build.addApartment(int(numberApartment), airHumidity, ambientAirTemperature, apartmentMaterial,
-                               roomQuantity,
-                               personQuantity, personClothing)
-
-        file.close()
+                           roomQuantity,
+                           personQuantity, isHabitability)
+        db.closeConnection()
 
     @staticmethod
     def loadNeighbors(build):
@@ -136,41 +153,59 @@ class BDD:
         file.close()
 
 class Menu:
+
+    @staticmethod
+    def Menu_getApartmentInfo():
+        os.system("clear")
+        numApartment = int(input("[+] Ingrese el número del apartamento que desea consultar: "))
+        if numApartment not in building.Apartments:
+            print("[!] El número de apartamento no existe, porfavor verifique de nuevo")
+            input("Press any key to continue...")
+        else:
+            building.Apartments[numApartment].getInfo()
+            input("Press any key to continue...")
+        Menu.initialMenu()
+
+
+        
+
     @staticmethod
     def initialMenu():
         os.system("clear")
         os.system("figlet \"Habitability Building\"")
+        print("[1] Obtener información de un apartamento")
+        print("[2] Actualizar información de un apartamento")
+
+
+        while True:
+            try:
+                opc = int(input("Ingrese una opción: "))
+                if opc == 1:
+                  Menu.Menu_getApartmentInfo()
+                elif  opc == 9:
+                    sys.exit(0)
+            except ValueError:
+                print("[!] Debe ingresar un número entero válido.")
+                input("Press any key to continue...")
+                Menu.initialMenu()
+            except KeyError:
+                print("[!] La opción elegida no es válida.")
+                input("Press any key to continue...")
+                Menu.initialMenu()
+
 
 
 
 if __name__ == '__main__':
-
      
     
-    db = Database.getInstance()
-
-    def sig_handler(sig, frame):
-        print("\n\n[!] Exiting ...")
-        print("[!] Closing Instance with the database")
-        print("Bye ")
-        db.closeConnection()
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, sig_handler)
-
-
+    
+    BDD.loadApartments(building)
     Menu.initialMenu()
-    a = input("Hola") 
+
     #    bd = Building()
-    #    BDD.loadApartments(bd)
     #    BDD.loadNeighbors(bd)
 
-    #    bd.Apartments[503].getInfo()
-    
-
-    result = db.executeQuery("SELECT * FROM habitantes")
-    print(result[2])
-    db.closeConnection()
 #    os.system("kitty +kitten icat /home/d3vjh/Desktop/fondos/Wallpaper.jpg")
     
 
