a212055d02a13c60b9a97766ce75128a4cded3a2
d3vjh <jdmorenob@udistrital.edu.co>
2023-05-25 01:08:35 -0500

    Create Person fixed

--- HabitabilityBuilding.py
+++ HabitabilityBuilding.py
@@ -25,9 +25,9 @@ class Database:
             Database.__instance = self
             self.connection = psycopg2.connect(
                 host="localhost",
-                database="habitable",  # This database must be updated later
-                user="root",
-                password="ballena12")
+                database="primeraprueba",  # This database must be updated later
+                user="postgres",
+                password="password")
 
     def executeQuery(self, query, args=None):
         """Method to execute a query on the database."""
@@ -54,6 +54,7 @@ class Database:
         """Method to close the database connection."""
         self.connection.close()
 
+
 """A global instance is created to access the database"""
 db = Database.getInstance()
 
@@ -73,51 +74,63 @@ signal.signal(signal.SIGINT, sig_handler)
 
 
 class Apartment:
-    def __init__(self, numberApartment, airHumidity, ambientAirTemperature, apartmentMaterial, roomQuantity,
-                 personQuantity, isHabitable):
-        self.numberApartment = numberApartment
-        self.airHumidity = airHumidity
-        self.ambientAirTemperature = ambientAirTemperature
-        self.apartmentMaterial = apartmentMaterial
-        self.roomQuantity = roomQuantity
-        self.personQuantity = personQuantity
+    def __init__(self, k_apartment, q_air_humidity, s_apartment_material, q_number_of_bedrooms,
+                 q_number_of_occupants, b_is_habitable):
+        self.k_apartment = k_apartment
+        self.q_air_humidity = q_air_humidity
+        self.s_apartment_material = s_apartment_material
+        self.q_number_of_bedrooms = q_number_of_bedrooms
+        self.q_number_of_occupants = q_number_of_occupants
         self.neighbors = []
-        self.isHabitable = isHabitable
+        self.residents = []
+        self.b_is_habitable = b_is_habitable
 
     def addNeighbor(self, neighbor):
         if neighbor not in self.neighbors:
             self.neighbors.append(neighbor)
 
+
+    def addResidents(self, resident):
+        if resident not in self.residents:
+            self.residents.append(resident)
+
+
     def getInfo(self):
-        print(f'[+]==== Info del apartamento: {self.numberApartment}====[+]\n\n'
+        print(f'[+]==== Info del apartamento: {self.k_apartment}====[+]\n\n'
               f''
-              f'[+] Es Habitable: {self.isHabitable} \n'
+              f'[+] Es Habitable: {self.b_is_habitable} \n'
               f'[+] Vecinos: {self.neighbors} \n'
-              f'[+] Humedad del aire:  {self.airHumidity} \n'
-              f'[+] Temperatura ambiental: {self.ambientAirTemperature} \n'
-              f'[+] Material del apartamento: {self.apartmentMaterial} \n'
-              f'[+] Tamaño del apartamento: {self.roomQuantity} \n'
-              f'[+] Cantidad de personas: {self.personQuantity} \n')
+              f'[+] Humedad del aire:  {self.q_air_humidity} \n'
+              f'[+] Material del apartamento: {self.s_apartment_material} \n'
+              f'[+] Tamaño del apartamento: {self.q_number_of_bedrooms} \n'
+              f'[+] Cantidad de personas: {self.q_number_of_occupants} \n'
+              f'[+] Personas residentes:')
+        for person in self.residents:
+            print(f'\t[-] Id: {person.k_person} \n'
+                  f'\t[-] Nombre: {person.s_name}\n'
+                  f'\t[-] Apellido: {person.s_last_name}\n'
+                  f'\t[-] Ropa: {person.s_clothing_type}\n'
+                  f'\t[-] Actividad: {person.s_activity}\n')
 
 
 class Building:
     def __init__(self):
         self.Apartments = {}
 
-    def addApartment(self, numberApartment, airHumidity, ambientAirTemperature, apartmentMaterial, roomQuantity,
-                     personQuantity, personClothing):
-        if numberApartment not in self.Apartments:
-            self.Apartments[numberApartment] = Apartment(numberApartment, airHumidity, ambientAirTemperature,
-                                                         apartmentMaterial, roomQuantity, personQuantity,
+    def addApartment(self, k_apartment, q_air_humidity, s_apartment_material, q_number_of_bedrooms,
+                     q_number_of_occupants, personClothing):
+        if k_apartment not in self.Apartments:
+            self.Apartments[k_apartment] = Apartment(k_apartment, q_air_humidity,
+                                                         s_apartment_material, q_number_of_bedrooms, q_number_of_occupants,
                                                          personClothing)
         else:
             print("The apartment already exists")
-            self.Apartments[numberApartment].getInfo()
+            self.Apartments[k_apartment].getInfo()
 
-    def addNeighbor(self, numberApartment1, numberApartment2):
-        if numberApartment1 in self.Apartments and numberApartment2 in self.Apartments:
-            self.Apartments[numberApartment1].addNeighbor(numberApartment2)
-            self.Apartments[numberApartment2].addNeighbor(numberApartment1)
+    def addNeighbor(self, k_apartment1, k_apartment2):
+        if k_apartment1 in self.Apartments and k_apartment2 in self.Apartments:
+            self.Apartments[k_apartment1].addNeighbor(k_apartment2)
+            self.Apartments[k_apartment2].addNeighbor(k_apartment1)
 
 
 
@@ -126,11 +139,14 @@ building = Building()
 
 
 class Person:
-    def __init__(self, name, lastName, apartment, clothes):
-        self.name = name
-        self.lastName = lastName
-        self.apartment = apartment
-        self.clothes = clothes
+    def __init__(self, k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity):
+
+        self.k_person = k_person
+        self.s_name = s_name
+        self.s_last_name = s_last_name
+        self.s_clothing_type = s_clothing_type
+        self.k_apartment = k_apartment
+        self.s_activity = s_activity
         
 
 
@@ -141,7 +157,7 @@ class Graphic:
         
         for i in building.Apartments:
             # agrega el nodo correspondiente en el grafo
-            G.add_node(i, number=building.Apartments[i].numberApartment, isHabitable=building.Apartments[i].isHabitable)
+            G.add_node(i, number=building.Apartments[i].k_apartment, b_is_habitable=building.Apartments[i].b_is_habitable)
             
             # itera sobre los vecinos del apartamento actual
             for neighbor_number in building.Apartments[i].neighbors:
@@ -154,13 +170,13 @@ class Graphic:
 
 #        for node in G.nodes():
 #            print(G.nodes[node]['number'])
-#            print(G.nodes[node]['isHabitable'])
+#            print(G.nodes[node]['b_is_habitable'])
 #            print(G.edges(node))
 #            print("[+]")
         Graphic.viewGraph()
 
     def viewGraph():
-        node_colors = ['green' if G.nodes[node]['isHabitable'] else 'red' for node in G.nodes()]
+        node_colors = ['green' if G.nodes[node]['b_is_habitable'] else 'red' for node in G.nodes()]
         pos = nx.spectral_layout(G)
         nx.draw(G, pos = pos, edge_color = 'white', with_labels=True, node_color=node_colors)
         plt.savefig('grafo.png', facecolor='#1A1B26')
@@ -189,14 +205,53 @@ class Graphic:
 
 
 class BDD:
+
+    @staticmethod
+    def isPerson(k_person):
+        try:
+            q = "SELECT k_person FROM person WHERE k_person = %s"
+            args = (k_person,)
+            result = db.executeQuery(q, args)
+            if result:
+                return True
+            else:
+                return False
+        except:
+            return False
 #Create
     @staticmethod
-    def createPerson(numApartment, n_name_person, n_lastname_person):
+    def createPerson(k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity):
         try:
-            q = "INSERT INTO person(k_id_apartment, n_name_person, n_lastname_person) VALUES (%s, %s, %s);"
-            args = (numApartment, n_name_person, n_lastname_person)
+            q = "INSERT INTO person(k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity) VALUES (%s, %s, %s, %s, %s, %s);"
+            args = (k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity)
             db.executeQuery(q, args)
             # Agregar el resto de parámetros && la condición NOT NULL
+
+            # DEBE llamar a addResidents()
+            person = Person(k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity)
+            building.Apartments[k_apartment].addResidents(person)
+
+            
+            # Obtenemos el valor actual de ocupantes en el edificio
+            q = "SELECT q_number_of_occupants FROM apartment WHERE k_apartment = %s"
+            args = (k_apartment,)
+            numAct = db.executeQuery(q, args)
+            num = list(numAct)
+            num = num[0][0]
+            print(num)
+            print(type(num))
+            # Aumentamos en uno, el valor de q_number_of_occupants
+            q = "UPDATE apartment SET q_number_of_occupants = %s WHERE k_apartment = %s"
+            num += 1
+            args = (num, k_apartment,)
+            db.executeQuery(q, args)
+
+            building.Apartments[k_apartment].q_number_of_occupants += 1
+
+
+
+
+            
         except psycopg2.Error as e:
             print("[BDD] Error en la creación de la persona: ", e)
             input("Press any key to continue...")
@@ -206,19 +261,17 @@ class BDD:
     @staticmethod
     def loadApartments(build):
         try:
-            result = db.executeQuery("SELECT k_apartment, q_air_humidity, q_ambient_air_humidity, s_apartment_material, q_number_of_bedrooms, q_number_of_occupants, b_is_habitable FROM apartment ORDER BY k_apartment;")
+            result = db.executeQuery("SELECT k_apartment, q_air_humidity, s_apartment_material, q_number_of_bedrooms, q_number_of_occupants, b_is_habitable FROM apartment ORDER BY k_apartment;")
 
             for i in range(len(result)):
-                numberApartment = result[i][0]
-                airHumidity = result[i][1]
-                ambientAirTemperature = result[i][2]
-                apartmentMaterial = result[i][3]
-                roomQuantity = result[i][4]
-                personQuantity = result[i][5]
-                isHabitability = result[i][6]
-                build.addApartment(int(numberApartment), airHumidity, ambientAirTemperature, apartmentMaterial,
-                               roomQuantity,
-                               personQuantity, isHabitability)
+                k_apartment = result[i][0]
+                q_air_humidity = result[i][1]
+                s_apartment_material = result[i][2]
+                q_number_of_bedrooms = result[i][3]
+                q_number_of_occupants = result[i][4]
+                b_is_habitable = result[i][5]
+                build.addApartment(int(k_apartment), q_air_humidity, s_apartment_material,
+                                    q_number_of_bedrooms, q_number_of_occupants, b_is_habitable)
         except psycopg2.Error as e:
             print("[BDD] Error en la operación de base de datos:", e)
             input("Press any key to continue...")
@@ -230,13 +283,35 @@ class BDD:
         try:
             result = db.executeQuery("SELECT k_apartment1, k_apartment2 FROM neighbor")
             for i in range(len(result)):
-                numberApartment1 = result[i][0]
-                numberApartment2 = result[i][1]
-                build.addNeighbor(numberApartment1, numberApartment2)
+                k_apartment1 = result[i][0]
+                k_apartment2 = result[i][1]
+                build.addNeighbor(k_apartment1, k_apartment2)
 
         except psycopg2.Error as e:
             print("[BDD ldNeighbors] Error en la operación de base de datos:", e)
             input("Press any key to continue...")
+
+
+    @staticmethod
+    def loadResidents(build):
+
+        for i in build.Apartments:
+            numApartment = int(build.Apartments[i].k_apartment)
+            q = "SELECT k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity  FROM person WHERE k_apartment = %s"
+            args = (numApartment,)
+            result = db.executeQuery(q, args)            
+            if result:
+                for j in range(len(result)):
+                    k_person = result[j][0]
+                    s_name = result[j][1]
+                    s_last_name = result[j][2]
+                    s_clothing_type = result[j][3]
+                    k_apartment = result[j][4]
+                    s_activity = result[j][5]
+                    person = Person(k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity)
+                    build.Apartments[i].addResidents(person)
+            del result
+            
 
 
 
@@ -247,17 +322,55 @@ class Menu:
     def Menu_createPerson():
         os.system("clear")
     
-        numApartment = int(input("[+] Ingrese el número del apartamento en el que va a estar: "))
-        while numApartment not in building.Apartments:
-            numApartment = int(input("[!] El número de apartamento no existe, porfavor verifique de nuevo: "))
-        n_name_person = input("[+] Ingrese el nombre de la persona: ")
-        while not re.match("^[A-Za-z]*$", n_name_person):
-            n_name_person = input("[!] Ingrese un nombre válido, sin números ni carácteres especiales: ")
-        n_lastname_person = input("[+] Ingrese el apellido de la persona: ")
-        while not re.match("^[A-Za-z]*$", n_lastname_person):
-            n_lastname_person =  input("[!] Ingrese un apellido válido, sin números ni carácteres especiales:")
+        k_apartment = int(input("[+] Ingrese el número del apartamento en el que va a estar: "))
+        while k_apartment not in building.Apartments:
+            k_apartment = int(input("[!] El número de apartamento no existe, porfavor verifique de nuevo: "))
 
-        BDD.createPerson(numApartment, n_name_person, n_lastname_person)
+        k_person = input("[+] Ingrese el número de identificación de la persona: ")
+        while BDD.isPerson(k_person):    
+            k_person = input("[+] Numero de identificación ya ingresado, porfavor verifique de nuevo: ")
+            
+        s_name = input("[+] Ingrese el nombre de la persona: ")
+        while not re.match("^[A-Za-z]*$", s_name):
+            s_name = input("[!] Ingrese un nombre válido, sin números ni carácteres especiales: ")
+
+        s_last_name = input("[+] Ingrese el apellido de la persona: ")
+        while not re.match("^[A-Za-z]*$", s_last_name):
+            s_last_name =  input("[!] Ingrese un apellido válido, sin números ni carácteres especiales:")
+        
+        clothing_type = {
+                '1' : 'Casual',
+                '2' : 'Desnudo'
+                }
+        print("[+] Selecciona el tipo de ropa: ")
+        print(f'\t1. Casual\n' 
+              f'\t2. Desnudo\n')
+        s_clothing_type = input("[$]> ")
+        while s_clothing_type not in clothing_type:
+            s_clothing_type = input("[!] Ingrese una opción válida Porfavor: ")
+        
+        s_clothing_type = clothing_type[s_clothing_type]
+
+        activity = {
+                '1' : 'Reposo',
+                '2' : 'Ligera',
+                '3' : 'Moderada',
+                '4' : 'Intensa'
+                }
+        print("[+] Selecciona la actividad a realizar: ")
+        print(f'\t1. Reposo\n'
+              f'\t2. Ligera\n'
+              f' \t3. Moderada\n'
+              f'  \t4. Intensa\n')
+        s_activity= input("[$]> ")
+        while s_activity not in activity:
+            s_activity= input("[!] Ingrese una opción válida Porfavor: ")
+        
+        s_activity = activity[s_activity]
+
+
+
+        BDD.createPerson(k_person, s_name, s_last_name, s_clothing_type, k_apartment, s_activity)
         Menu.initialMenu()
             # Ingrese el resto de valores 
        
@@ -321,6 +434,7 @@ class Menu:
 if __name__ == '__main__':
     BDD.loadApartments(building)
     BDD.loadNeighbors(building)
+    BDD.loadResidents(building)
     Menu.initialMenu()
 
     #    bd = Building()
